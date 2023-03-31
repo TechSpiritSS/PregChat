9f9e3c560bc86d921e5a1f1ceccf89c0cd53667f
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2023-03-31 22:57:13 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+// import reactLogo from './assets/react.svg';
 import './App.css';
-import KomunicateChat from './components/chat/chat';
+// import KomunicateChat from './components/chat/chat';
 
 function App() {
   const [count, setCount] = useState(0);
